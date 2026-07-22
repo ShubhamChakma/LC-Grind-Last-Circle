3eeca41bd628a100411bd77fe480f9bdf857375b
ShubhamChakma <159429702+ShubhamChakma@users.noreply.github.com>
2026-07-22 23:43:23 +0530

    Time: 142 ms (10.27%), Space: 172.8 MB (5.02%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
-    int minScore(int n, vector<vector<int>>& roads) {
-        vector<int> root(n + 1);
-        iota(root.begin(), root.end(), 0);
+    int findparent(int node, vector<int>& parent) {
+        if(parent[node]==node){
+            return node;
+        }      
+        parent[node]= findparent(parent[node], parent);
+        return parent[node];
 
-        auto find = [&](this auto& self, int i) -> int {
-            return root[i] == i ? i : root[i] = self(root[i]);
-        };
+    }
+    int minScore(int n, vector<vector<int>>&roads ){
+        vector <int> parent(n+1);
+        for( int i=1; i<=n; i++){
+            parent[i]=i;
 
-        for (auto& r : roads)
-            root[find(r[0])] = find(r[1]);
+        }
+        for (auto road:roads){
+            int city1=road[0];
+            int city2=road[1];
+            int parent1= findparent(city1, parent);
+            int parent2= findparent(city2, parent);
+            parent[parent1]=parent2;
 
-        int res = 10001;
-        for (auto& r : roads)
-            if (find(r[0]) == find(1))
-                res = min(res, r[2]);
 
-        return res;
+        }
+        int group=findparent(1, parent);
+        int answer = INT_MAX;
+        for (auto road:roads){
+            int city1=road[0];
+            int distance=road[2];
+            if(findparent(city1, parent)==group){
+                answer=min(answer,distance);
+
+            }
+
+        }
+        return answer;
+
     }
 };
